557404733ec8e150df674d2e078cf8bf54e6e105
Turwaith <nicolasbaur@turwaith.dev>
2024-08-26 11:34:45 +0200

    Adds button for testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonToggle } from "@angular/material/button-toggle";
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    MatIconModule,
+    MatGridListModule, MatButtonToggle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
